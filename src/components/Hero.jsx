163a72125292f7a4f2fe1b3fcb31ac932595da1f
Abdullah Ragab <abdullahragab460@gmail.com>
2025-12-06 15:30:44 +0200

    update images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -92,7 +92,7 @@ const Hero = () => {
               }}
               className="w-64 h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-full overflow-hidden border-4 border-primary-600 dark:border-primary-400 shadow-2xl"
             >
-              <img src={profilePic} />
+              <img src={profilePic}   alt="Abdullah Ragab" className="w-full h-full object-cover"/>
             </motion.div>
             <motion.div
               animate={{
